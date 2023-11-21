8195164abdd64b2dd41c7ba4264ea32e8c5351da
Saeera <sa7739640@gmail.com>
2023-11-21 20:07:34 +0500

    Modified BankAccount.dart

--- bin/bankaccount.dart
+++ bin/bankaccount.dart
@@ -1,5 +1,6 @@
 class BankAccount{
      double balance=0.0;
+     int _accountId = 0;
 
   set checkBalance(double amount){
     if(amount>0){
@@ -10,4 +11,10 @@ class BankAccount{
 
   }
   double get checkBalance=>balance;
+
+  set accoundId(int id){
+    _accountId = id;
+  }
+
+  int get accoundId => _accountId;
 }
